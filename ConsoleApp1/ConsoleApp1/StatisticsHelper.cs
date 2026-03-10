c8dd2ff6be685e0f6fe62560bd1e1140b4c71c01
Anton Ovcharenko <toha.ovcharenko@gmail.com>
2026-03-10 15:24:02 +0100

    Added CalculateAverage method in the StatisticsHelper.cs

--- ConsoleApp1/ConsoleApp1/StatisticsHelper.cs
+++ ConsoleApp1/ConsoleApp1/StatisticsHelper.cs
@@ -2,8 +2,15 @@
 
 public class StatisticsHelper
 {
-    public static void CalculateAverage(int[] values)
+    public static int CalculateAverage(int[] numbers)
     {
+        int sum = 0;
         
+        foreach (var number in numbers)
+        {
+            sum += number;
+        }
+        
+        return sum / numbers.Length;
     }
 }
