5504aa38976a232d912900ad9b29e94a1aa6c3af
Anton Ovcharenko <toha.ovcharenko@gmail.com>
2026-03-10 14:54:09 +0100

    Add user input handling

--- ConsoleApp1/ConsoleApp1/Program.cs
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -1,3 +1,7 @@
-﻿// See https://aka.ms/new-console-template for more information
+﻿Console.WriteLine("Enter a number:");
+string input = Console.ReadLine();
 
-Console.WriteLine("Hello, World!");
+if (int.TryParse(input, out int number))
+{
+    Console.WriteLine("You entered: " + number);
+}
